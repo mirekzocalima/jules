eade599cab18bc2c4204e8bd18e9b609e21d6a64
MirekZOcalima <damir.herman@gmail.com>
2025-05-31 00:05:38 -0700

    refactor: Update import paths to reflect project renaming from jules_core to jules_backend

--- backend/jules_backend/__init__.py
+++ backend/jules_backend/__init__.py
@@ -1,9 +1,9 @@
 __version__ = "0.0.1"
 
-from jules_core.logging import setup_logging
-from jules_core.model import list_available_cable_types, NautilusModel
-from jules_core.operability import get_operability_heatmap, get_operability_matrix
-from jules_core.rao import RAOCalculator
+from jules_backend.logging import setup_logging
+from jules_backend.model import list_available_cable_types, NautilusModel
+from jules_backend.operability import get_operability_heatmap, get_operability_matrix
+from jules_backend.rao import RAOCalculator
 
 from cables.loader import NautilusCables
 from vessels.loader import NautilusVessels
